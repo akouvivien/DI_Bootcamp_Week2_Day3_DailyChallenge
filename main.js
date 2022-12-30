adbb35f390a6aab7d0cae3ae391a17cba03305ab
akouvivien <akouvivien@gmail.com>
2022-12-30 12:28:11 +0000

    add comment and index

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 // Daily Challenge: Stars
-// Last updated : March 30th 2021
-
-
 // What You Will Learn :
 // For loops and nested for loops
 
@@ -9,12 +6,19 @@
 // Instructions
 // Write a JavaScript program that recreates the pattern below.
 // Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
-// Do this Daily Challenge by youself, without looking at the answers on the internet.
-
 
 let nombre = 6
 let etoile =""
 for (let i = 0; i < nombre; i++) {
     etoile = etoile.concat(" * ")
     console.log(etoile)
+}
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+const nomb = 6;
+for (let i = 0; i < nomb; i++) {
+    let etoile ="";
+  for (let j = 0; j < i+1; j++) {
+    etoile = etoile.concat(" * "); 
+  }
+  console.log(etoile)
 }
